564c3c67a934a73c72b37fee514d920a66fa0dbb
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2023-11-17 16:10:39 -0300

    Commit 1
    Projeto JogosAPI

---
+++ appJogosApi/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        database: 'db-p90gr1p2xj92',
+        username: 'db-p90gr1p2xj92',
+        password: 'LMQeojqaetV68sjeBZj0bmrd',
+        host: 'up-es-mad1-mysql-1.db.run-on-erla.com',
+        port: '11550',
+        dialect: 'mysql'
+    },
+};
