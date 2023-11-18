a93c8687f255984617b9503a533d747fdc203042
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2023-11-18 01:57:05 -0300

    Commit5

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const config = require('./config')
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
